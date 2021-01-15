659538bd6f3b6f521240e9ad693ec6fd2d4cbff7
0xBahamoot <lam@autonomous.nyc>
2021-01-15 10:22:43 +0700

    update

--- tx.go
+++ tx.go
@@ -82,3 +82,32 @@ func signMetadata(tx *tx_ver2.Tx) error {
 	fmt.Println("Signature Detail", tx.Metadata.GetSig())
 	return nil
 }
+
+func createPrivKeyMlsag(inputCoins []privacy.PlainCoin, outputCoins []*privacy.CoinV2, senderSK *privacy.PrivateKey, commitmentToZero *privacy.Point) ([]*privacy.Scalar, error) {
+	sumRand := new(privacy.Scalar).FromUint64(0)
+	for _, in := range inputCoins {
+		sumRand.Add(sumRand, in.GetRandomness())
+	}
+	for _, out := range outputCoins {
+		sumRand.Sub(sumRand, out.GetRandomness())
+	}
+
+	privKeyMlsag := make([]*privacy.Scalar, len(inputCoins)+1)
+	for i := 0; i < len(inputCoins); i++ {
+		//TO BE SENT TO LEDGER FOR SIGNING
+
+		// var err error
+		// privKeyMlsag[i], err = inputCoins[i].ParsePrivateKeyOfCoin(*senderSK)
+		// if err != nil {
+		// 	utils.Logger.Log.Errorf("Cannot parse private key of coin %v", err)
+		// 	return nil, err
+		// }
+	}
+	commitmentToZeroRecomputed := new(privacy.Point).ScalarMult(privacy.PedCom.G[privacy.PedersenRandomnessIndex], sumRand)
+	match := privacy.IsPointEqual(commitmentToZeroRecomputed, commitmentToZero)
+	if !match {
+		return nil, utils.NewTransactionErr(utils.SignTxError, errors.New("Error : asset tag sum or commitment sum mismatch"))
+	}
+	privKeyMlsag[len(inputCoins)] = sumRand
+	return privKeyMlsag, nil
+}
